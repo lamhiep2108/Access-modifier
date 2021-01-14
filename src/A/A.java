83bab1414f05f491a57237756b295eea9dde09fa
Admin <hiepnothip@gmail.com>
2021-01-14 14:20:36 +0700

    'l

---
+++ src/A/A.java
@@ -0,0 +1,14 @@
+package A;
+
+public class A {
+    public void msg(){
+        System.out.println("hello");
+
+    }
+}
+// 1.private --thêm getter,setter
+//2.default--thêm public
+//3.protected--thêm extend
+//4.public=đc truy cập full
+//
+//7
---
+++ src/B/B.java
@@ -0,0 +1,8 @@
+package B;
+import A.*;
+public class B  {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.msg();
+    }
+}
